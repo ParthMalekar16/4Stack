f4aa31086d9f828e6db45a327ac5ce55aab65a1f
Parth Malekar <parthmalekar60@gmail.com>
2026-04-20 21:27:07 +0530

    Navbar added

--- frontend/components/navbar.tsx
+++ frontend/components/navbar.tsx
@@ -1,11 +1,17 @@
 export default function Navbar(){
-    return (<nav className="w-full px-8 py-4 flex justify-between items-center bg-white/60 backdrop-blur-md shadow-sm">
-          <h1 className="text-xl font-bold">4Stack 🚀</h1>
+    return (<nav className="w-full px-20 py-3 flex items-center bg-white text-black/80 backdrop-blur-md shadow-sm">
+          <h1 className="text-xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-pink-500 to-purple-500">4Stack </h1>
 
-          <div className="flex gap-6 text-sm font-medium">
-            <a href="#">Home</a>
-            <a href="#">About</a>
-            <a href="#">Contact</a>
+          <div className="flex gap-6 text-sm font-medium mx-auto">
+            <a href="#">Deals</a>
+            <a href="#">Compare Products</a>
+            <a href="#">Deals Under</a>
+          </div>
+
+          <div className="flex gap-4">
+            <button className="px-4 py-2 text-sm hover:text-red-500 transition bg-blue-500 text-white rounded-full">
+              Login/Sign Up
+            </button>
           </div>
         </nav>
     );
